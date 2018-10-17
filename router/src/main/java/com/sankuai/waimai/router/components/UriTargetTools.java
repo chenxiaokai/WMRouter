d92e1664b5fbab9e379726b47d10d5d5297aa93b
chenxiaokai <chenxiaokai@finupgroup.com>
2018-10-17 17:13:07 +0800

    增加注释让代码更可读

--- router/src/main/java/com/sankuai/waimai/router/components/UriTargetTools.java
+++ router/src/main/java/com/sankuai/waimai/router/components/UriTargetTools.java
@@ -43,6 +43,11 @@ public class UriTargetTools {
     }
 
     private static boolean isValidActivityClass(Class clazz) {
+        /*
+         *  a.isAssignableFrom(b) 返回true的条件
+         *      1. a对象所对应类信息是b对象所对应的类信息的父类或者是父接口，简单理解即a是b的父类或接口
+         *      2. a对象所对应类信息与b对象所对应的类信息相同，简单理解即a和b为同一个类或同一个接口
+         */
         return clazz != null && Activity.class.isAssignableFrom(clazz)
                 && !Modifier.isAbstract(clazz.getModifiers());
     }
